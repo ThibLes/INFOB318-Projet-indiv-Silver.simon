5ecd9b7810fcecc2c99207a4ffe7995e50a16415
Thibaut Lesage <91668259+ThibLes@users.noreply.github.com>
2024-02-28 16:28:58 +0100

    ajout liste nom + début de l'ecran game

--- app/src/main/java/com/example/silversimon_projetindiv/Game.kt
+++ app/src/main/java/com/example/silversimon_projetindiv/Game.kt
@@ -1,10 +1,12 @@
 package com.example.silversimon_projetindiv
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class Game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +20,14 @@ class Game : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        val textViewPrenomPatient = findViewById<TextView>(R.id.textViewPrenomPatient)
+
+        // Accéder aux mêmes préférences partagées pour lire le prénom
+        val sharedPreferences = getSharedPreferences("PrenomPatient", Context.MODE_PRIVATE)
+        val prenomPatient = sharedPreferences.getString("prenom", "Inconnu")
+
+        // Définir le texte de textViewPrenomPatient pour inclure le prénom du patient
+        textViewPrenomPatient.text = getString(R.string.bonjour_patient, prenomPatient)
 }
 }
